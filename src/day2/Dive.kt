d001da9dba23fc8a8141e0795937cfa32ec38242
Martin Eckardt <martin.eckardt@moo-solutions.de>
2021-12-06 10:51:47 +0100

    Add solution for day 2

---
+++ src/day2/Dive.kt
@@ -0,0 +1,42 @@
+package day2
+
+class Dive(val commands: List<String>) {
+
+    fun getDepth() : Int {
+        var depth = 0
+        var forward = 0
+
+        commands.forEach {
+            val command = it.split(' ')
+            if (command[0] == "forward") {
+                forward += command[1].toInt()
+            } else if(command[0] == "down") {
+                depth += command[1].toInt()
+            } else if (command[0] == "up") {
+                depth -= command[1].toInt()
+            }
+        }
+        return depth * forward
+    }
+
+    fun getFinalDepth() : Int {
+        var aim = 0
+        var depth = 0
+        var forward = 0
+
+        commands.forEach {
+            val command = it.split(' ')
+            if (command[0] == "forward") {
+                val f = command[1].toInt()
+                forward += f
+                depth += aim * f
+            } else if(command[0] == "down") {
+                aim += command[1].toInt()
+            } else if (command[0] == "up") {
+                aim -= command[1].toInt()
+            }
+        }
+        return depth * forward
+    }
+
+}
